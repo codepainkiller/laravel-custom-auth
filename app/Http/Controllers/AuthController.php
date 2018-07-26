d18e27d88a7d1ace3d0ffa9ec03217f09d171707
Martin <martincruz.cs@gmail.com>
2018-07-26 09:00:41 -0500

    remove repeat method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,11 +16,6 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function showRegisterForm()
-    {
-        return view('auth.register');
-    }
-
     public function logout()
     {
         auth()->logout();
